b0cd94f434bb0cd552c639caba67d3b687316310
thedavidmeister <thedavidmeister@gmail.com>
2023-04-19 13:42:15 +0400

    test round trip against slow impl

--- test/LibMemoryKV.t.sol
+++ test/LibMemoryKV.t.sol
@@ -4,15 +4,35 @@ pragma solidity ^0.8.18;
 import "forge-std/Test.sol";
 
 import "../src/LibMemoryKV.sol";
+import "./LibMemoryKVSlow.sol";
 
 contract LibMemoryKVTest is Test {
     function testRoundTrip(uint256[] memory kvs_) public {
+        // We hit gas limits pretty easily in this test for "large" sets.
+        vm.assume(kvs_.length < 50);
         vm.assume(kvs_.length % 2 == 0);
+
         MemoryKV kv_ = MemoryKV.wrap(0);
+
+        uint256[] memory slowKVs_ = new uint256[](0);
         for (uint256 i_ = 0; i_ < kvs_.length; i_ += 2) {
-            kv_ = LibMemoryKV.setVal(kv_, MemoryKVKey.wrap(kvs_[i_]), MemoryKVVal.wrap(kvs_[i_ + 1]));
+            uint256 k_ = kvs_[i_];
+            uint256 v_ = kvs_[i_ + 1];
+
+            kv_ = LibMemoryKV.setVal(kv_, MemoryKVKey.wrap(k_), MemoryKVVal.wrap(v_));
+            slowKVs_ = LibMemoryKVSlow.set(slowKVs_, k_, v_);
         }
 
-        assertEq(kvs_, LibMemoryKV.toUint256Array(kv_));
+        uint256[] memory roundKVs_ = LibMemoryKV.toUint256Array(kv_);
+        assertEq(slowKVs_.length, roundKVs_.length);
+
+        for (uint256 i_ = 0; i_ < slowKVs_.length; i_ += 2) {
+            uint256 k_ = slowKVs_[i_];
+            (bool slowExists_, uint256 slowVal_) = LibMemoryKVSlow.get(slowKVs_, k_);
+            (bool roundExists_, uint256 roundVal_) = LibMemoryKVSlow.get(roundKVs_, k_);
+            assertEq(slowExists_, true);
+            assertEq(roundExists_, true);
+            assertEq(slowVal_, roundVal_);
+        }
     }
 }
--- test/LibMemoryKVSlow.sol
+++ test/LibMemoryKVSlow.sol
@@ -1,17 +1,33 @@
 // SPDX-License-Identifier: CAL
 pragma solidity ^0.8.18;
 
+import "sol.lib.memory/LibUint256Array.sol";
+
 library LibMemoryKVSlow {
-    function get(uint256[] memory kvs_, k_) internal pure returns (bool, uint256) {
+    function exists(uint256[] memory kvs_, uint256 k_) internal pure returns (bool, uint256) {
         for (uint256 i_ = 0; i_ < kvs_.length; i_ += 2) {
             if (kvs_[i_] == k_) {
-                return (true, kvs_[i_]);
+                return (true, i_);
             }
         }
         return (false, 0);
     }
 
-    function set(uint256[] memory kvs_, k_, v_) internal pure returns (uint256[] memory) {
+    function get(uint256[] memory kvs_, uint256 k_) internal pure returns (bool, uint256) {
+        (bool exists_, uint256 index_) = exists(kvs_, k_);
+        return (exists_, exists_ ? kvs_[index_] : 0);
+    }
 
+    function set(uint256[] memory kvs_, uint256 k_, uint256 v_) internal pure returns (uint256[] memory) {
+        (bool exists_, uint256 index_) = exists(kvs_, k_);
+        if (exists_) {
+            kvs_[index_ + 1] = v_;
+            return kvs_;
+        } else {
+            uint256[] memory kv_ = new uint256[](2);
+            kv_[0] = k_;
+            kv_[1] = v_;
+            return LibUint256Array.unsafeExtend(kvs_, kv_);
+        }
     }
 }
