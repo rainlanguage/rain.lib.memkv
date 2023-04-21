e499e8c1f37bee93b5f2b3620c037a7de766c137
thedavidmeister <thedavidmeister@gmail.com>
2023-04-21 15:55:34 +0400

    wip on saturate test

---
+++ test/LibMemoryKV.saturate.t.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: CAL
+pragma solidity ^0.8.18;
+
+import "forge-std/Test.sol";
+import "../src/LibMemoryKV.sol";
+
+contract LibMemoryKVSaturateTest is Test {
+    function testSaturate(
+        uint256 seed
+    ) public {
+        uint256[60] kvs = [
+            0,
+            0,
+        ]
+    }
+}
